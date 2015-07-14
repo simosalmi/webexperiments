c714539a90ac8b972f3439cbb9e6349c77e6db1b
Simone Salmistraro <simo.salmi@gmail.com>
2015-07-14 23:50:23 +0100

    after writing the firts v of the post

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,48 @@
+
 'use strict';
 
-console.log('ciao');
+function goodFunction() {	
+	var arr = [1, 2, 3, 4, 5];
+	var index = null;
+	for (index in arr) {
+	    var  myFunc = function() {};
+		function fuckingFunction() {
+			console.log('motherfucker');
+		};
+		fuckingFunction();
+	}
+	
+};
+
+goodFunction();
+
+console.log('hi from main');
+
+var isStrict = (function() { return !this; })();
+
+console.log('is strict in main? ' + isStrict);
+
+var isStrict = (function() { return !this; })();
+
+console.log('is strict second time? ' + isStrict);
+
+// code not in strict mode
+
+var nonStrictFunction = function() {
+	// code not in strict mode
+}
+
+var myFunction = function() {
+	'use strict';
+	// ...
+	// code in strict mode
+	var f = function() {
+		// code in strict mode;
+	};
+	
+	f(); // strict code call
+	
+	nonStrictFunction(); // non strict code call
+}
+
+// code not in strict mode
