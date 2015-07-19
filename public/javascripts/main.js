124f8301bb5f368791a737cd25453fad65fd2019
Simone Salmistraro <simo.salmi@gmail.com>
2015-07-19 01:23:08 +0100

    after post completion

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,24 +1,10 @@
-// see how it changes the array itself
-var arr1 = [1, 3, 2];
+var arr = [1, 0, 1, 2, 21, 3, 5, 8, 13];
 
-arr1.forEach(function(el, i, arr) {
-	arr[i] = 2*el;
-	arr.push(i + 1);
+var max = arr.reduceRight(function getMax(prev, curr, i, array) {
+	if(curr > prev) 
+		return curr;
+	
+	return prev; 	
 });
 
-console.log(arr1); // logs [2, 6, 4, 1, 2, 3]
-// it doesn't iterate on elements added/removed after the call to forEach
-
-// see how it skips undefined indexes
-var arr2 = [];
-
-arr2[1] = 0;
-
-arr2[3] = 1;
-
-arr2.forEach(function(el, i, arr) {
-	console.log(i, el);
-}); 
-// logs:
-// 1 0
-// 3 1 
+console.log(max);
